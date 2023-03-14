de45451e04363522672ed2136864d09995972f3d
Tica Sunshine <vranic.tica@gmail.com>
2023-03-14 20:25:08 +0100

    setup router

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import { NewNote } from './NewNote';
+
 
 function App() {
   return (
-   <h1>Hi</h1>
+    <Container className='my-4'>
+   <Routes>
+    <Route path='/' element={<h1>Hi</h1>}/>
+    <Route path='/new' element={<NewNote/>}/>
+    <Route path='*' element={<Navigate to="/" />}/> {/*go back to home page when someone enter wrong url*/}
+    <Route path='/:id'>
+      <Route index element={<h1>Show</h1>}/>
+      <Route path='edit' element={<h1>Edit</h1>}/>
+    </Route>
+   </Routes>
+   </Container>
   )
 }
 
