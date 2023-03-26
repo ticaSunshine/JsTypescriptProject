3af62aaa4e6ced2ab85f0c8d5e9c20e10d2841b0
Tica Sunshine <vranic.tica@gmail.com>
2023-03-26 10:41:20 +0200

    setup for creation logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,24 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { useMemo } from 'react';
 import { Container } from 'react-bootstrap'
 import {Routes, Route, Navigate} from 'react-router-dom'
 import { NewNote } from './NewNote';
+import { useLocalStorage } from './useLocalStorage';
+import { uuid } from 'uuidv4';
 
 export  type Note = {
   id: string
   & NoteData
+}
+
+export type RawNote = {
+  id: string;
+}& RawNoteData
+
+export  type RawNoteData = {
+  title: string
+  markdown: string
+  tagIds: string[]
 }
 
 export  type NoteData = {
@@ -19,11 +32,26 @@ export type Tag = {
   label: string
 }
 function App() {
+
+  const [notes, setNotes]=useLocalStorage<RawNote[]>('NOTES', []);
+  const [tags, setTags]=useLocalStorage<Tag[]>('TAGS', [])
+
+  const notesWithTags = useMemo(() => {
+    return notes.map(note => {
+      return{...note, tags: tags.filter(tag => note.tagIds.includes(tag.id))}
+    })
+  }, [notes,tags]);
+
+  function onCreateNote({tags, ...data}: NoteData){
+    setNotes(prevNotes => {
+      return [...prevNotes, {...data, id: uuid(), tagIds: tags.map(tag => tag.id)}]
+    })
+  }
   return (
     <Container className='my-4'>
    <Routes>
     <Route path='/' element={<h1>Hi</h1>}/>
-    <Route path='/new' element={<NewNote/>}/>
+    <Route path='/new' element={<NewNote onSubmit={onCreateNote}/>}/>
     <Route path='*' element={<Navigate to="/" />}/> {/*go back to home page when someone enter wrong url*/}
     <Route path='/:id'>
       <Route index element={<h1>Show</h1>}/>
@@ -35,3 +63,7 @@ function App() {
 }
 
 export default App;
+function uuidV4(): string {
+  throw new Error('Function not implemented.');
+}
+
